7414a0e08df94e9de3ca70937ef8f73a1ad361be
tobiasvinther <tobi626i@stud.kea.dk>
2021-10-09 14:21:43 +0200

    first commit

---
+++ src/main/java/com/example/herokuspringtest/HerokuSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokuspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuSpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/herokuspringtest/HerokuSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokuspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
